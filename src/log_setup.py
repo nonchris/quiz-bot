308ed1b129225a26015890639e7cc16ee0b2a857
nonchris <git@chris-ge.de>
2021-09-26 15:27:32 +0200

    Update log format

--- src/log_setup.py
+++ src/log_setup.py
@@ -11,16 +11,14 @@ if not os.path.exists('data/'):
     os.mkdir('data/')
 
 # set logging format
-formatter = logging.Formatter("[{asctime}] [{levelname}] [{name}] {message}", style="{")
+formatter = logging.Formatter("[{asctime}] [{levelname}] [{module}.{funcName}] {message}", style="{")
 
 # logger for writing to file
 file_logger = logging.FileHandler('data/events.log')
-file_logger.setLevel(logging.INFO)  # everything into the logging file
 file_logger.setFormatter(formatter)
 
 # logger for console prints
 console_logger = logging.StreamHandler()
-console_logger.setLevel(logging.WARNING)  # only important stuff to the terminal
 console_logger.setFormatter(formatter)
 
 # get new logger
